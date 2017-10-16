e7ac9c117b7ca397f7f09d936a7dc9fa2cc73718
shcherbinina-yelyzaveta <scherbininaliza@gmail.com>
2017-10-17 00:06:41 +0300

    Second task (Person) completed

--- src/com/company/task8/Person.java
+++ src/com/company/task8/Person.java
@@ -6,21 +6,29 @@ package com.company.task8;
  * б) методы move() и talk().
  * в) Добавьте два конструктора  - Person() и Person(fullName, age).
  * Создайте два объекта этого класса. Один объект инициализируется конструктором Person(), другой - Person(fullName, age).
- *
+ * <p>
  * Created by Liza on 16.10.2017.
  */
 public class Person {
     String fullName;
     int age;
 
-    Person(){
+    Person() {
         this.fullName = "Somebody";
         this.age = 30;
     }
 
-    Person(String fullName,int age){
+    Person(String fullName, int age) {
         this.fullName = fullName;
         this.age = age;
+    }
+
+    void move() {
+        System.out.println(this.fullName + " move");
+    }
+
+    void talk() {
+        System.out.println(this.fullName + " talk");
     }
 
 
--- src/com/company/task8/PersonMain.java
+++ src/com/company/task8/PersonMain.java
@@ -6,8 +6,12 @@ package com.company.task8;
 public class PersonMain {
     public static void main(String[] args) {
         Person person1 = new Person();
-        Person person2 = new Person("Liza Shcherbinina",19);
+        Person person2 = new Person("Liza Shcherbinina", 19);
         System.out.println(person1.fullName + " " + person1.age);
         System.out.println(person2.fullName + " " + person2.age);
+        person1.move();
+        person2.move();
+        person1.talk();
+        person2.talk();
     }
 }
