d4a49093feec501c0ea3d5cb9d67fc421a9d0a8f
shcherbinina-yelyzaveta <scherbininaliza@gmail.com>
2017-10-16 20:09:06 +0300

    Added class Person and main for this

---
+++ src/com/company/task8/Person.java
@@ -0,0 +1,27 @@
+package com.company.task8;
+
+/**
+ * Создать класс Person, который содержит:
+ * a) поля fullName, age.
+ * б) методы move() и talk().
+ * в) Добавьте два конструктора  - Person() и Person(fullName, age).
+ * Создайте два объекта этого класса. Один объект инициализируется конструктором Person(), другой - Person(fullName, age).
+ *
+ * Created by Liza on 16.10.2017.
+ */
+public class Person {
+    String fullName;
+    int age;
+
+    Person(){
+        this.fullName = "Somebody";
+        this.age = 30;
+    }
+
+    Person(String fullName,int age){
+        this.fullName = fullName;
+        this.age = age;
+    }
+
+
+}
---
+++ src/com/company/task8/PersonMain.java
@@ -0,0 +1,13 @@
+package com.company.task8;
+
+/**
+ * Created by Liza on 16.10.2017.
+ */
+public class PersonMain {
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        Person person2 = new Person("Liza Shcherbinina",19);
+        System.out.println(person1.fullName + " " + person1.age);
+        System.out.println(person2.fullName + " " + person2.age);
+    }
+}
