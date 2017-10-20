e6b551074697933e1ef1f6d5b1b1fbadc35a93ab
shcherbinina-yelyzaveta <scherbininaliza@gmail.com>
2017-10-20 21:02:12 +0300

    task 9, first task completed (class Phone)

--- src/com/company/task8/Phone.java
+++ src/com/company/task8/Phone.java
@@ -11,19 +11,25 @@ package com.company.task8;
  * д) Добавить конструктор без параметров.
  * е) Вызвать из конструктора с параметрами конструктор по умолчанию.
  * ж) Добавьте перегруженный метод receiveCall, который принимает два параметра - имя звонящего и номер телефона звонящего.
+ * з) Создать метод sendMessage  с аргументами переменной длины. Данный метод принимает на вход номера телефонов,
+ *    которым будет отправлено сообщение. Метод выводит на консоль номера этих телефонов.
+ * и) Изменить класс Phone в соответствии с концепцией JavaBean.
+ * к) Реализовать подсчет количества созданных телефонов с помощью статической переменной.
  * Вызвать этот метод.
- *
+ * <p>
  * Created by Liza on 16.10.2017.
  */
 public class Phone {
-    String number;
-    String model;
-    int weight;
+    private String number;
+    private String model;
+    private int weight;
+    private static int count = 0;
 
     Phone() {
         this.number = "0000";
         this.model = "something";
         this.weight = 10;
+        count++;
     }
 
     Phone(String number, String model, int weight) {
@@ -33,15 +39,46 @@ public class Phone {
         this.weight = weight;
     }
 
+    public static int getCount() {
+        return count;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
     void receiveCall(String name) {
         System.out.println("Звонит " + name);
     }
 
-    String getPhoneNumber() {
-        return this.number;
-    }
-
     void receiveCall(String name, String number) {
         System.out.println("Звонит " + name + " номер: " + number);
+    }
+
+    void sendMassage(String... number) {
+        System.out.println("Отправить сообщение:");
+        for (String tmp : number) {
+            System.out.println(tmp);
+        }
     }
 }
--- src/com/company/task8/PhoneMain.java
+++ src/com/company/task8/PhoneMain.java
@@ -9,28 +9,31 @@ public class PhoneMain {
         Phone obj2 = new Phone();
         Phone obj3 = new Phone();
 
-        obj1.number = "5152544";
-        obj1.model = "Lenovo K5";
-        obj1.weight = 35;
+        obj1.setNumber("5152544");
+        obj1.setModel("Lenovo K5");
+        obj1.setWeight(35);
         obj1.receiveCall("Liza");
-        System.out.println(obj1.getPhoneNumber());
+        System.out.println(obj1.getNumber());
 
-        obj2.number = "4445442";
-        obj2.model = "Iphone 5s";
-        obj3.weight = 20;
+        obj2.setNumber("4445442");
+        obj2.setModel("Iphone 5s");
+        obj2.setWeight(20);
         obj2.receiveCall("Vitya");
-        System.out.println(obj2.getPhoneNumber());
+        System.out.println(obj2.getNumber());
 
-        obj3.number = "7745895";
-        obj3.model = "Iphone 6s";
-        obj3.weight = 25;
+        obj3.setNumber("7745895");
+        obj3.setModel("Iphone 6s");
+        obj3.setWeight(25);
         obj3.receiveCall("Bogdan");
-        System.out.println(obj3.getPhoneNumber());
+        System.out.println(obj3.getNumber());
 
         Phone obj4 = new Phone();
-        System.out.println(obj4.model + " " + obj4.getPhoneNumber());
+        System.out.println(obj4.getModel() + " " + obj4.getNumber());
 
         Phone obj5 = new Phone("7254489", "Samsung S8", 25);
-        obj5.receiveCall("Alex", obj5.getPhoneNumber());
+        obj5.receiveCall("Alex", obj5.getNumber());
+        obj5.sendMassage(obj1.getNumber(), obj2.getNumber(), obj3.getNumber(), obj4.getNumber());
+
+        System.out.println("Количество объектов класса Phone = " + Phone.getCount());
     }
 }
