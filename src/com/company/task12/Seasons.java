6ed3dc4c6f35600a14a25bcdf6b539d7a6a4d924
shcherbinina-yelyzaveta <scherbininaliza@gmail.com>
2017-11-01 11:57:03 +0300

    task Seasons done (lesson 12)

--- src/com/company/task12/Seasons.java
+++ src/com/company/task12/Seasons.java
@@ -4,7 +4,12 @@ package com.company.task12;
  * Created by student on 30.10.2017.
  */
 public enum Seasons {
-    WINTER(-10), SPRING(10), SUMMER(20), AUTUMN(8);
+    WINTER(-10), SPRING(10), AUTUMN(8), SUMMER(20) {
+        @Override
+        public String getDescription() {
+            return "Жаркое время года";
+        }
+    };
 
     private int temperature;
 
@@ -14,5 +19,9 @@ public enum Seasons {
 
     public int getTemperature() {
         return temperature;
+    }
+
+    public String getDescription() {
+        return "Холодное время года";
     }
 }
--- src/com/company/task12/SeasonsMain.java
+++ src/com/company/task12/SeasonsMain.java
@@ -14,7 +14,7 @@ public class SeasonsMain {
         System.out.println("Seasons:");
         Seasons[] seasons = Seasons.values();
         for (Seasons season : seasons) {
-            System.out.print(season + " ");
+            System.out.println(season + " " + season.getDescription() + " " + season.getTemperature() + "\u00B0C");
         }
     }
 
@@ -33,5 +33,7 @@ public class SeasonsMain {
                 System.out.println("I like summer!");
                 break;
         }
+        System.out.println(myFavoriteSeason + " " + myFavoriteSeason.getDescription() + " " +
+                myFavoriteSeason.getTemperature() + "\u00B0C");
     }
 }
