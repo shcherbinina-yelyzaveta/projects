5dadc4d073e7f572f04c02301aea1fb44fc3c130
shcherbinina-yelyzaveta <scherbininaliza@gmail.com>
2017-11-03 21:15:30 +0200

    fixed bugs in task Sells (lesson 12)

--- src/com/company/task12/Shells.java
+++ src/com/company/task12/Shells.java
@@ -19,16 +19,7 @@ public class Shells {
         Double d2 = Double.valueOf("3.3154324154854");
         double d3 = Double.parseDouble("451.4");
         Double d4 = 134.583;
-        System.out.println("valueOf: " + d1 + " " + d2);
-        System.out.println("parseXxx: " + d3);
-        System.out.println("autobox: " + d4);
-
-        System.out.println("int " + d2.intValue());
-        System.out.println("short " + d2.shortValue());
-        System.out.println("long " + d2.longValue());
-        System.out.println("byte " + d2.byteValue());
-        System.out.println("float " + d2.floatValue());
-        System.out.println("double " + d2.doubleValue());
+        print(d1,d2,d3,d4);
 
         String s = Double.toString(d3);
         System.out.println(s);
@@ -41,16 +32,7 @@ public class Shells {
         Integer i2 = Integer.valueOf("356455");
         int i3 = Integer.parseInt("451");
         Integer i4 = 134;
-        System.out.println("valueOf: " + i1 + " " + i2);
-        System.out.println("parseXxx: " + i3);
-        System.out.println("autobox: " + i4);
-
-        System.out.println("int " + i2.intValue());
-        System.out.println("short " + i2.shortValue());
-        System.out.println("long " + i2.longValue());
-        System.out.println("byte " + i2.byteValue());
-        System.out.println("float " + i2.floatValue());
-        System.out.println("double " + i2.doubleValue());
+        print(i1,i2,i3,i4);
 
         String s = Integer.toString(i3);
         System.out.println(s);
@@ -63,21 +45,13 @@ public class Shells {
         Byte b2 = Byte.valueOf("127");
         byte b3 = Byte.parseByte("4");
         Byte b4 = 13;
-        System.out.println("valueOf: " + b1 + " " + b2);
-        System.out.println("parseXxx: " + b3);
-        System.out.println("autobox: " + b4);
-
-        System.out.println("int " + b2.intValue());
-        System.out.println("short " + b2.shortValue());
-        System.out.println("long " + b2.longValue());
-        System.out.println("byte " + b2.byteValue());
-        System.out.println("float " + b2.floatValue());
-        System.out.println("double " + b2.doubleValue());
+        print(b1,b2,b3,b4);
 
         String s = Byte.toString(b3);
         System.out.println(s);
         System.out.println("-------------------------------");
     }
+
 
     public static void shortMethod() {
         System.out.println("-------------SHORT-------------");
@@ -85,16 +59,7 @@ public class Shells {
         Short s2 = Short.valueOf("32767");
         short s3 = Short.parseShort("451");
         Short s4 = 134;
-        System.out.println("valueOf: " + s1 + " " + s2);
-        System.out.println("parseXxx: " + s3);
-        System.out.println("autobox: " + s4);
-
-        System.out.println("int " + s2.intValue());
-        System.out.println("short " + s2.shortValue());
-        System.out.println("long " + s2.longValue());
-        System.out.println("byte " + s2.byteValue());
-        System.out.println("float " + s2.floatValue());
-        System.out.println("double " + s2.doubleValue());
+        print(s1,s2,s3,s4);
 
         String s = Short.toString(s3);
         System.out.println(s);
@@ -107,16 +72,7 @@ public class Shells {
         Long l2 = Long.valueOf("3545455522233");
         long l3 = Long.parseLong("451");
         Long l4 = 134L;
-        System.out.println("valueOf: " + l1 + " " + l2);
-        System.out.println("parseXxx: " + l3);
-        System.out.println("autobox: " + l4);
-
-        System.out.println("int " + l2.intValue());
-        System.out.println("short " + l2.shortValue());
-        System.out.println("long " + l2.longValue());
-        System.out.println("byte " + l2.byteValue());
-        System.out.println("float " + l2.floatValue());
-        System.out.println("double " + l2.doubleValue());
+        print(l1,l2,l3,l4);
 
         String s = Long.toString(l3);
         System.out.println(s);
@@ -129,19 +85,23 @@ public class Shells {
         Float f2 = Float.valueOf("3.11111111");
         float f3 = Float.parseFloat("451");
         Float f4 = 134.0F;
-        System.out.println("valueOf: " + f1 + " " + f2);
-        System.out.println("parseXxx: " + f3);
-        System.out.println("autobox: " + f4);
-
-        System.out.println("int " + f2.intValue());
-        System.out.println("short " + f2.shortValue());
-        System.out.println("long " + f2.longValue());
-        System.out.println("byte " + f2.byteValue());
-        System.out.println("float " + f2.floatValue());
-        System.out.println("double " + f2.doubleValue());
+        print(f1,f2,f3,f4);
 
         String s = Float.toString(f3);
         System.out.println(s);
         System.out.println("-------------------------------");
+    }
+
+    public static void print(Number b1, Number b2, Number b3, Number b4) {
+        System.out.println("valueOf: " + b1 + " " + b2);
+        System.out.println("parseXxx: " + b3);
+        System.out.println("autobox: " + b4);
+
+        System.out.println("int " + b2.intValue());
+        System.out.println("short " + b2.shortValue());
+        System.out.println("long " + b2.longValue());
+        System.out.println("byte " + b2.byteValue());
+        System.out.println("float " + b2.floatValue());
+        System.out.println("double " + b2.doubleValue());
     }
 }
