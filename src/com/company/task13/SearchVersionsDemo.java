209e4c9a855b0d4796510fc18070b7b8d40fca74
shcherbinina-yelyzaveta <scherbininaliza@gmail.com>
2017-11-06 21:19:40 +0200

    task Versions Java done

---
+++ src/com/company/task13/SearchVersionsDemo.java
@@ -0,0 +1,18 @@
+package com.company.task13;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by student on 06.11.2017.
+ */
+public class SearchVersionsDemo {
+    public static void main(String[] args) {
+        String str = "Version: Java5, Java 6, Java  7, Java 8";
+        Pattern pattern = Pattern.compile("Java\\s*\\d+");
+        Matcher m = pattern.matcher(str);
+        while (m.find()) {
+            System.out.println(m.group());
+        }
+    }
+}
