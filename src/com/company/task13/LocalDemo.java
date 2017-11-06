65d5e9cb668cde631fe62ef086e7b63430b75f90
shcherbinina-yelyzaveta <scherbininaliza@gmail.com>
2017-11-06 21:31:02 +0200

    начало работы над заданием

---
+++ src/com/company/task13/LocalDemo.java
@@ -0,0 +1,17 @@
+package com.company.task13;
+
+import java.util.Locale;
+
+/**
+ * Created by student on 06.11.2017.
+ */
+public class LocalDemo {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(String language, String country) {
+        Locale l = new Locale(language, country);
+    }
+
+}
