22a976c768087390c38e71418d3f64913c4ca7ba
shcherbinina-yelyzaveta <scherbininaliza@gmail.com>
2017-11-19 20:28:29 +0200

    fixed bugs

--- src/com/company/task17/Task5.java
+++ src/com/company/task17/Task5.java
@@ -3,7 +3,7 @@ package com.company.task17;
 import java.util.HashSet;
 import java.util.Set;
 
-public class task5 {
+public class Task5 {
     public static void main(String[] args) {
         String string = "1, 2, 3, 4, 4, 5";
         Set<Character> set = new HashSet<>();
