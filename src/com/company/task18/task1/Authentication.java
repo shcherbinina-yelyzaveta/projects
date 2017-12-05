b71553a32bdc00d4cf74e36bdf38bfbab9d62d42
shcherbinina-yelyzaveta <scherbininaliza@gmail.com>
2017-12-05 13:58:47 +0200

    task 1 done (lesson 18)

--- src/com/company/task18/task1/Authentication.java
+++ src/com/company/task18/task1/Authentication.java
@@ -1,31 +1,38 @@
 package com.company.task18.task1;
 
+import java.util.regex.Pattern;
+
 /**
  * Created by student on 24.11.2017.
  */
 public class Authentication {
     public static void main(String[] args) {
-        try {
-            System.out.println(isCorrect("login1", "password1", "password1"));
-            System.out.println(isCorrect("login1loginloginlogin", "password1", "password1"));
-            System.out.println(isCorrect("login1", "password1password1password1", "password1password1password1"));
-
-            System.out.println(isCorrect("login1", "password1", "password2"));
-            System.out.println(isCorrect("login1loginloginlogin", "password1", "password2"));
-        } catch (WrongLoginException e) {
-            e.printStackTrace();
-        } catch (WrongPasswordException e) {
-            e.printStackTrace();
-        }
+        System.out.println(isCorrect("login_1", "password_1", "password_1"));
+        System.out.println(isCorrect("login1loginloginlogin", "password1", "password1"));
+        System.out.println(isCorrect("login1", "password1password1password1", "password1password1password1"));
+        System.out.println(isCorrect("login1", "password1", "password2"));
+        System.out.println(isCorrect("login1loginloginlogin", "password1", "password2"));
+        System.out.println(isCorrect("login/03", "password", "password"));
+        System.out.println(isCorrect("login_1", "password**", "password**"));
+        System.out.println(isCorrect("login_1", "password**", "password"));
+        System.out.println(isCorrect("login//1", "password**", "password"));
+        System.out.println(isCorrect("login1", "password9101112131415", "password9101112131415"));
     }
 
-    public static boolean isCorrect(String login, String password, String confPassword) throws WrongLoginException, WrongPasswordException {
-        if (login.length() > 20) {
-            throw new WrongLoginException("login exp");
+
+    public static boolean isCorrect(String login, String password, String confPassword) {
+        try {
+            if (!Pattern.compile("\\w{1,20}").matcher(login).matches()) {
+                throw new WrongLoginException("Логин неверный");
+            }
+            if (!password.equals(confPassword) || !Pattern.compile("\\w{1,20}").matcher(password).matches()) {
+                throw new WrongPasswordException("Пароль неверный");
+            }
+        } catch (WrongLoginException | WrongPasswordException e) {
+            System.out.print(e.getMessage() + " ");
+            return false;
         }
-        if (password.length() > 20 || !password.equals(confPassword)) {
-            throw new WrongPasswordException("password exp");
-        }
+        System.out.print("Login: " + login + " Password: " + password + " ");
         return true;
     }
 }
